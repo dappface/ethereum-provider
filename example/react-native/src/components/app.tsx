8b7b5578ef84faf28b6d9fa9850eb61c2b72f27f
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-08-05 14:16:14 +0900

    inject injectable.js

--- example/react-native/src/components/app.tsx
+++ example/react-native/src/components/app.tsx
@@ -1,7 +1,12 @@
-import code from 'dappface-provider-web-example/dist/index.json'
-import React from 'react'
+import { JsonRpcMethod } from '@dappface/ethereum-provider'
+import React, { useRef } from 'react'
+import { INFURA_PROJECT_ID } from 'react-native-dotenv'
 import { WebView } from 'react-native-webview'
+import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes'
 import styled from 'styled-components/native'
+
+// @ts-ignore
+import code from '../../dist/index.json'
 
 // TODO
 // - establish websocket connection to a node
@@ -32,13 +37,43 @@ const html = `
 `
 
 export function App() {
+  function handleMessage(e: WebViewMessageEvent): void {
+    console.log(e.nativeEvent.data)
+  }
+
+  const ref = useRef<WebView | null>(null)
+
+  function handleLoad() {
+    const ws = new WebSocket(
+      `wss://rinkeby.infura.io/ws/v3/${INFURA_PROJECT_ID}`
+    )
+    ws.onopen = () => {
+      if (!ref.current) {
+        return
+      }
+
+      const msg = JSON.stringify({
+        jsonrpc: '2.0',
+        method: JsonRpcMethod.DFSocketConnected
+      })
+
+      ref.current.injectJavaScript(`
+        window.ethereum.on('connect', () => alert('connected'));
+        window.ethereum.postMessage('${msg}');
+        true;
+      `)
+    }
+  }
+
   return (
     <Container>
       <StyledWebView
+        ref={ref}
         originWhitelist={['*']}
         source={{ html }}
-        onMessage={console.log}
-        injectedJavaScript={code['index.native.js']}
+        onMessage={handleMessage}
+        onLoad={handleLoad}
+        injectedJavaScript={code['injectable.js']}
       />
     </Container>
   )
