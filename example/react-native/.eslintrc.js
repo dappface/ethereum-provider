7d9ce0650867a3bace588cfce9b6b6f9f8a84b4b
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-08-06 10:45:53 +0900

    setup eslint

--- example/react-native/.eslintrc.js
+++ example/react-native/.eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off'
+  }
+}
