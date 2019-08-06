f40fe3665a98455b2d5d32311301cfd27def37dc
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-08-06 10:34:55 +0900

    fix eslint config

--- example/web/.eslintrc.js
+++ example/web/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'plugin:prettier/recommended',
@@ -8,6 +9,9 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true
+    },
     sourceType: 'module'
   },
   rules: {
@@ -15,6 +19,12 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     // details: https://github.com/typescript-eslint/typescript-eslint/issues/201
-    '@typescript-eslint/explicit-member-accessibility': 'off'
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-use-before-define': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
