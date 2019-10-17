112e30c33bd0971a2f439f3f94b8ead6924a6e5e
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-10-17 17:54:55 +0200

    remove tslint comments

--- example/web/src/components/index.ts
+++ example/web/src/components/index.ts
@@ -4,7 +4,6 @@ import Web3 from 'web3'
 export { App } from './app'
 
 declare global {
-  // tslint:disable-next-line:interface-name
   interface Window {
     ethereum: EthereumProvider
     web3: Web3
